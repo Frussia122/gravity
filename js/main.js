3971d8016ca98a17178d85da7e4d1ab4c585e0d3
Amidgard <44028334+Frussia122@users.noreply.github.com>
2021-09-03 20:57:40 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+$('.services__slider').slick({
+  variableWidth: true,
+  arrows: true,
+  prevArrow: '<button id="prev" type="button" class="arrows arrow__left"><</button>',
+  nextArrow: '<button id="next" type="button" class="arrows arrow__right">></button>'
+});
+$('.testimonials__slider').slick({
+  fade: true,
+  arrows: true,
+  prevArrow: '<button id="prev" type="button" class="arrows arrows--testimonials arrow__left"><</button>',
+  nextArrow: '<button id="next" type="button" class="arrows arrows--testimonials arrow__right">></button>'
+});
+"use strict";
+
+console.log('maxgraph');
+/**
+  * название функции
+  *
+  * @param {number} first - первое число
+  * @returns {number}
+  */
+"use strict";
+//# sourceMappingURL=main.js.map
